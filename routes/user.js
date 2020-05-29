a77d37f529d85f4c2b3274b58298ff5ad1eabb7f
ncomerci <ncomerci@itba.edu.ar>
2020-05-29 15:59:48 -0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -7,11 +7,13 @@ module.exports = class UserRoute {
     }
 
     async initialize(app) {
-        app.route('/user')
+        app.route('/user/')
+            .get(this.getUser.bind(this))
+
+        app.route('/user/all')
             .get(this.getUsers.bind(this));
 
         app.route('/user/:id')
-            .get(this.getUser.bind(this))
             .put(this.modifyUser.bind(this))
             .delete(this.delUser.bind(this));
 
@@ -31,16 +33,15 @@ module.exports = class UserRoute {
 
     async getUser(req, res) {
         try {
-            const {
-                id
-            } = req.params;
-            const user = await this.server.db('t_user').select(["a_name", "a_email"]).where({
-                a_user_id: id
-            });
-            if (user.length == 1)
-                res.status(200).json(message.fetch('user', user));
-            else
-                res.status(404).json(message.notFound('user', id));
+            const{a_user_id, a_name, a_email} = req.user;
+            const user = [
+                {
+                    id: a_user_id,
+                    name: a_name,
+                    email: a_email
+                }
+            ]
+            res.status(200).json(message.fetch('user', user));
         } catch (error) {
             console.log(error);
         }
