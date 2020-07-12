2bee3d3b9afa3b95706ce6c54ceb068f91b71bfc
ncomerci <ncomerci@itba.edu.ar>
2020-07-12 16:58:41 -0300

    addDB

--- fillDB.sql
+++ fillDB.sql
@@ -5,6 +5,8 @@ insert into t_type values(DEFAULT, 'pasta', 'https://firebasestorage.googleapis.
 insert into t_type values(DEFAULT, 'ensalada', 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ftypes%2Fensalada.jpg?alt=media&token=b385cb89-00b6-442d-9f84-7e033bbc5f8b');
 insert into t_type values(DEFAULT, 'mexicana', 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ftypes%2Ftacos.jpg?alt=media&token=a218f633-5862-4b79-ada0-3421b7494ceb');
 insert into t_type values(DEFAULT, 'sushi', 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ftypes%2Fsushi.jpg?alt=media&token=3d8ee9f1-bd93-481b-b311-98f5105ee93b');
+insert into t_type values(DEFAULT, 'minutas', 'https://neuquen24horas.com/wp-content/uploads/2019/04/436282-24a00532-effe-43a2-a025-27d206875cfb.jpg');
+insert into t_type values(DEFAULT, 'pastas secas', 'https://saborgourmet.com/wp-content/uploads/pasta-al-huevo-rellena.jpg');
 insert into t_type values(DEFAULT, 'carne', 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ftypes%2Fcarne.jpg?alt=media&token=3ff4c005-7f5b-4d08-a81c-bef9dfa3db84');
 -- select * from t_type;
 
@@ -13,25 +15,201 @@ insert into t_characteristic values(DEFAULT, 'vegano');
 insert into t_characteristic values(DEFAULT, 'vegetariano');
 insert into t_characteristic values(DEFAULT, 'celiaco');
 insert into t_characteristic values(DEFAULT, 'diabetico');
+insert into t_characteristic values(DEFAULT, 'intolerante a la lactosa');
+insert into t_characteristic values(DEFAULT, 'hipertenso');
+
 -- select * from t_characteristic;
 
 
-insert into t_ingredient values(DEFAULT, 'tomate');
-insert into t_ingredient values(DEFAULT, 'queso');
-insert into t_ingredient values(DEFAULT, 'pollo');
-insert into t_ingredient values(DEFAULT, 'carne');
-insert into t_ingredient values(DEFAULT, 'lentejas');
-insert into t_ingredient values(DEFAULT, 'pan');
-insert into t_ingredient values(DEFAULT, 'lechuga');
-insert into t_ingredient values(DEFAULT, 'zanahoria');
-insert into t_ingredient values(DEFAULT, 'cebolla');
-insert into t_ingredient values(DEFAULT, 'ketchup');
-insert into t_ingredient values(DEFAULT, 'tortilla');
-insert into t_ingredient values(DEFAULT, 'frijoles');
-insert into t_ingredient values(DEFAULT, 'palta');
-insert into t_ingredient values(DEFAULT, 'filadelfia');
-insert into t_ingredient values(DEFAULT, 'salmon');
-insert into t_ingredient values(DEFAULT, 'arroz');
+INSERT INTO t_ingredient VALUES(DEFAULT, "Aceite de soja");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Aceite de maíz");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Aceite de oliva");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Aceite de trufa");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Crema de leche");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Yogur");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Leche");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Helado");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Queso");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cuajada");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Mantequilla");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Habichuelas");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Garbanzos");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Guisante");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Habas");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Lentejas");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Soja");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Setas");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Hongos");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Boleto");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Champiñon");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Huitlacoche");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Niscalo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Seta shiitake");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Seta de cardo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Seta de chopo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Trufa");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Patata");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Lechuga");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Acelga");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Alcachofa");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Batata");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Berenjena");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Berro");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Brocoli");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Brecol");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Calabacin");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Calabaza");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cardo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cebolla");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cebolla caramelizada");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cebolleta");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Coles");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Coles de Bruselas");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Coliflor");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Endivia");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Tomate");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Zanahoria");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Escarola");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Esparrago");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Espinaca");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Hinojo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Judias");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Maiz");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Palmito");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pepino");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pimiento");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Puerro");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Remolacha");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Albaricoque");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Caquis");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Aguacate");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cereza");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Mango");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Melocotones");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Nectarina");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Ciruela");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Fruta del bosque");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Frutos secos");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Membrillo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Peras");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Manzanas");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Uvas");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Fruta seca");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Frambuesa");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Fresa");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Granada");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Higos");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Kiwi");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Limon");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Mandarina");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Melon");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Naranja");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Piña");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Platano");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pomelo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Sandia");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cereal");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Harina");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pan");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Arroz");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pasta");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Atun");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Anchoa");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Bacalao");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Besugo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Bonito");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Boqueron");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Caballa");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Chicharro");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Corvina");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Dorada");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Lenguado");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Lubina");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Merluza");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Mero");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Palometa");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pejerrey");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Rape");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Rodaballo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Salmon");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Sardina");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Trucha");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Camaron");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cigala");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Gamba");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Langostino");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Percebe");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Almeja");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Calamar");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Mejillon");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Ostra");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pulpo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Sepia");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Vieira");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Carnes");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cordero");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Ovejas");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Ternera");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Buey");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Caballo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cabra");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cerdo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Vaca");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Avestruz");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Paloma");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Perdiz");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pato");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pavo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Oca");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Gallina");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Pollo");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Clara de huevo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Yema de huevo");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Bison");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Conejo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Liebre");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Oso");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Ciervo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Jabali");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Patos");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Faisanes");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Oca salvaje");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Ancas de rana");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Caracoles");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cerveza");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Sidra");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Licores");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Vino blanco");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Vino caliente");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Vino tinto");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Vino rosado");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Vino dulce");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Whisky");
+
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cafe");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cola");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Agua");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Agua mineral");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Agua gaseosa");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Zumo");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Te");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Horchata");
+INSERT INTO t_ingredient VALUES(DEFAULT, "Cerveza sin alcohol");
 -- select * from t_ingredient;
 
 
@@ -51,7 +229,7 @@ insert into t_restaurant values(DEFAULT, 'Crustacio Cascarudo', 2, 2, 2, 2, 'Fon
 -- select * from t_restaurant;
 
 
-insert into t_food values(DEFAULT, 'BigMac', 'Hamburguesa clásica', 3, 2, 1, 2, 2, 1, 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ffoods%2FBigMac.jpg?alt=media&token=91942492-25c1-47b8-ae13-840a0d51c779');
+insert into t_food values(DEFAULT, 'BigMac', 'Hamburguesa clasica', 3, 2, 1, 2, 2, 1, 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ffoods%2FBigMac.jpg?alt=media&token=91942492-25c1-47b8-ae13-840a0d51c779');
 insert into t_food values(DEFAULT, 'Burrito de Fideos', 'Mex mex go!', 5, 5, 5, 5, 5, 6, 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ffoods%2FBurritoDeFideos.jpg?alt=media&token=e1f1d51c-345c-4ad8-b2d2-a1473da64854');
 insert into t_food values(DEFAULT, 'Kangreburger', 'La mejor debajo del mar. Formula SECRETA!', 5, 2, 2, 3, 2, 8, 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ffoods%2FCangreBurger.jpg?alt=media&token=6ac4357e-7e06-418b-81d4-b00568b82a6d');
 insert into t_food values(DEFAULT, 'Crispy Burger', 'super CROCANTEE', 2, 2, 2, 2, 2, 2, 'https://firebasestorage.googleapis.com/v0/b/foodwayz-e9a26.appspot.com/o/images%2Ffoods%2FCrispyBurger.jpg?alt=media&token=0733845d-8f24-4dbd-a305-6e3f6bd23522');
@@ -139,6 +317,8 @@ INSERT INTO t_food_has_characteristic VALUES(11, 4);
 INSERT INTO t_food_has_characteristic VALUES(12, 4);
 INSERT INTO t_food_has_characteristic VALUES(12, 3);
 INSERT INTO t_food_has_characteristic VALUES(13, 4);
+INSERT INTO t_food_has_characteristic VALUES(4, 5);
+INSERT INTO t_food_has_characteristic VALUES(5, 6);
 -- select * from t_food_has_characteristic;
 
 
@@ -196,6 +376,60 @@ INSERT INTO t_food_has_ingredient VALUES(13, 9);
 INSERT INTO t_food_has_ingredient VALUES(13, 11);
 INSERT INTO t_food_has_ingredient VALUES(13, 12);
 INSERT INTO t_food_has_ingredient VALUES(13, 13);
+INSERT INTO t_food_has_ingredient VALUES(1, 17);
+INSERT INTO t_food_has_ingredient VALUES(1, 18);
+INSERT INTO t_food_has_ingredient VALUES(1, 19);
+INSERT INTO t_food_has_ingredient VALUES(1, 20);
+INSERT INTO t_food_has_ingredient VALUES(1, 21);
+INSERT INTO t_food_has_ingredient VALUES(1, 22);
+INSERT INTO t_food_has_ingredient VALUES(2, 23);
+INSERT INTO t_food_has_ingredient VALUES(3, 24);
+INSERT INTO t_food_has_ingredient VALUES(3, 25);
+INSERT INTO t_food_has_ingredient VALUES(3, 26);
+INSERT INTO t_food_has_ingredient VALUES(3, 27);
+INSERT INTO t_food_has_ingredient VALUES(3, 28);
+INSERT INTO t_food_has_ingredient VALUES(3, 29);
+INSERT INTO t_food_has_ingredient VALUES(4, 30);
+INSERT INTO t_food_has_ingredient VALUES(4, 31);
+INSERT INTO t_food_has_ingredient VALUES(4, 32);
+INSERT INTO t_food_has_ingredient VALUES(4, 33);
+INSERT INTO t_food_has_ingredient VALUES(4, 34);
+INSERT INTO t_food_has_ingredient VALUES(4, 35);
+INSERT INTO t_food_has_ingredient VALUES(5, 36);
+INSERT INTO t_food_has_ingredient VALUES(5, 37);
+INSERT INTO t_food_has_ingredient VALUES(5, 38);
+INSERT INTO t_food_has_ingredient VALUES(6, 39);
+INSERT INTO t_food_has_ingredient VALUES(6, 40);
+INSERT INTO t_food_has_ingredient VALUES(6, 41);
+INSERT INTO t_food_has_ingredient VALUES(6, 42);
+INSERT INTO t_food_has_ingredient VALUES(6, 43);
+INSERT INTO t_food_has_ingredient VALUES(6, 44);
+INSERT INTO t_food_has_ingredient VALUES(7, 45);
+INSERT INTO t_food_has_ingredient VALUES(7, 46);
+INSERT INTO t_food_has_ingredient VALUES(7, 47);
+INSERT INTO t_food_has_ingredient VALUES(7, 48);
+INSERT INTO t_food_has_ingredient VALUES(7, 49);
+INSERT INTO t_food_has_ingredient VALUES(7, 50);
+INSERT INTO t_food_has_ingredient VALUES(8, 51);
+INSERT INTO t_food_has_ingredient VALUES(8, 52);
+INSERT INTO t_food_has_ingredient VALUES(8, 53);
+INSERT INTO t_food_has_ingredient VALUES(8, 54);
+INSERT INTO t_food_has_ingredient VALUES(9, 55);
+INSERT INTO t_food_has_ingredient VALUES(9, 56);
+INSERT INTO t_food_has_ingredient VALUES(10, 57);
+INSERT INTO t_food_has_ingredient VALUES(11, 58);
+INSERT INTO t_food_has_ingredient VALUES(11, 59);
+INSERT INTO t_food_has_ingredient VALUES(11, 60);
+INSERT INTO t_food_has_ingredient VALUES(12, 61);
+INSERT INTO t_food_has_ingredient VALUES(12, 62);
+INSERT INTO t_food_has_ingredient VALUES(12, 63);
+INSERT INTO t_food_has_ingredient VALUES(12, 64);
+INSERT INTO t_food_has_ingredient VALUES(13, 65);
+INSERT INTO t_food_has_ingredient VALUES(13, 66);
+INSERT INTO t_food_has_ingredient VALUES(13, 67);
+INSERT INTO t_food_has_ingredient VALUES(13, 68);
+INSERT INTO t_food_has_ingredient VALUES(13, 69);
+INSERT INTO t_food_has_ingredient VALUES(13, 70);
 -- select * from t_food_has_ingredient;
 
 
